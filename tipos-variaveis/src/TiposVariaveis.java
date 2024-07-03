13272caa612f5277ecefbe9e49025892e633357d
Gabriel Prezzoti <gabrielbprezzoti@outlook.com>
2024-07-03 14:46:06 -0300

    tipos e variaveis

--- tipos-variaveis/src/TiposVariaveis.java
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -1,5 +1,15 @@
 public class TiposVariaveis {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        double salarioMinimo = 2.500;
+        int cep = 53271050; //se começar com 0 pode ter que ser outro tipo
+        long cpf = 714248834123L; //se começar com 0 pode ter que ser outro tipo
+        float pi = 3.14F; 
+        //tipo long termina com L e tipo float termina com F
+
+        //Vartáveis e constantes
+        final double VALOR_DE_PI = 3.14; //colocando "final" definimos o valor como constante.
+        
+
+        
     }
 }
